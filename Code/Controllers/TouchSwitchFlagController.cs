21988cc50d4d24de3960c3b6082a8e6bb7a99c32
Xaphan67 <cedric.falda@gmail.com>
2023-01-13 22:11:35 +0100

    feature: Touch Switch Flag Controller

---
+++ Code/Controllers/TouchSwitchFlagController.cs
@@ -0,0 +1,42 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.XaphanHelper.Controllers
+{
+    [CustomEntity("XaphanHelper/TouchSwitchFlagController")]
+    class TouchSwitchFlagController : Entity
+    {
+        string flag;
+
+        public TouchSwitchFlagController(EntityData data, Vector2 position) : base(data.Position + position)
+        {
+            Tag = Tags.TransitionUpdate;
+            flag = data.Attr("flag");
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+            if (!string.IsNullOrEmpty(flag) && !SceneAs<Level>().Session.GetFlag("switches_" + SceneAs<Level>().Session.Level))
+            {
+                SceneAs<Level>().Session.SetFlag(flag, false);
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (!string.IsNullOrEmpty(flag))
+            {
+                foreach (Switch switchCmp in SceneAs<Level>().Tracker.GetComponents<Switch>())
+                {
+                    if (switchCmp.Finished)
+                    {
+                        SceneAs<Level>().Session.SetFlag(flag, true);
+                    }
+                }
+            }
+        }
+    }
+}
