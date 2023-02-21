50515e91d978652559b30e9ce8a83f59e94631cc
Xaphan67 <cedric.falda@gmail.com>
2023-02-21 05:04:05 +0100

    feature: Fixes and improvements to Work Robots

--- Code/Entities/WorkRobot.cs
+++ Code/Entities/WorkRobot.cs
@@ -26,32 +26,75 @@ namespace Celeste.Mod.XaphanHelper.Entities
 
         private Coroutine TurnAroundRoutine = new();
 
+        private Coroutine ActiveRoutine = new();
+
+        private bool active;
+
+        private string flag;
+
+        private string directory;
+
         public WorkRobot(EntityData data, Vector2 offset) : base(data.Position + offset, 10f, 17f, false)
         {
+            Tag = Tags.TransitionUpdate;
             SurfaceSoundIndex = 7;
             size = data.Attr("size", "Medium");
+            flag = data.Attr("flag");
+            speed = data.Int("speed", 20);
+            directory = data.Attr("directory");
+            if (string.IsNullOrEmpty(directory))
+            {
+                directory = "objects/XaphanHelper/WorkRobot";
+            }
             OnDashCollide = OnDashed;
             width = size == "Small" ? 10 : size == "Medium" ? 15 : 20;
             height = size == "Small" ? 17 : size == "Medium" ? 26 : 35;
             float x = size == "Small" ? 3f : size == "Medium" ? 0f : -2f;
             float y = size == "Small" ? 7f : size == "Medium" ? -2f : -11f;
             Collider = new Hitbox(width, height, x, y);
-            Add(sprite = new Sprite(GFX.Game, "enemies/Xaphan/WorkRobot-" + size + "/"));
+            Add(sprite = new Sprite(GFX.Game, directory + "/" + size + "/"));
             sprite.Position = new Vector2(-8, -16);
-            sprite.AddLoop("idle", "idle", 0f);
+            sprite.AddLoop("idle", "turn", 0f, 7);
             sprite.AddLoop("walk", "walk", 0.1f);
             sprite.Add("turn", "turn", 0.08f);
-            sprite.Play("walk");
+            sprite.Add("activate", "turn", 0.08f, 7, 7, 6, 5, 4, 3, 2, 1, 0);
+            sprite.Add("deactivate", "turn", 0.08f, 0, 1, 2, 3, 4, 5, 6, 7, 7);
             goLeft = sprite.FlipX = data.Bool("startWalkLeft", false);
-            speed = 20;
             Add(new ClimbBlocker(edge: true));
+            sprite.OnFrameChange = onFrameChange;
+            sprite.Play("walk");
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+            if (!string.IsNullOrEmpty(flag))
+            {
+                if (SceneAs<Level>().Session.GetFlag(flag))
+                {
+                    Add(new Coroutine(Activate()));
+                }
+                else
+                {
+                    Add(ActiveRoutine = new Coroutine(DeActivate(true)));
+                }
+            }
+            else
+            {
+                active = true;
+            }
         }
 
         public override void Update()
         {
             base.Update();
-            if (!TurnAroundRoutine.Active)
+            if (ActiveRoutine.Active && !pushed)
             {
+                Speed.X = 0;
+            }
+            if (!TurnAroundRoutine.Active && !ActiveRoutine.Active && active)
+            {
+                sprite.FlipX = goLeft;
                 if (!pushed && CollideCheck<Solid>(Position + Vector2.UnitY))
                 {
                     Rectangle LeftHit = size == "Small" ? new Rectangle((int)Position.X + 2, (int)Position.Y + 24, 1, 1) : size == "Medium" ? new Rectangle((int)Position.X - 1, (int)Position.Y + 24, 1, 1) : new Rectangle((int)Position.X - 3, (int)Position.Y + 24, 1, 1);
@@ -80,6 +123,27 @@ namespace Celeste.Mod.XaphanHelper.Entities
                     }
                 }
             }
+            if (!string.IsNullOrEmpty(flag) && !TurnAroundRoutine.Active && !ActiveRoutine.Active)
+            {
+                if (!active)
+                {
+                    if (SceneAs<Level>().Session.GetFlag(flag))
+                    {
+                        Add(ActiveRoutine = new Coroutine(Activate()));
+                    }
+                }
+                else
+                {
+                    if (!SceneAs<Level>().Session.GetFlag(flag))
+                    {
+                        if (TurnAroundRoutine.Active)
+                        {
+                            TurnAroundRoutine.Cancel();
+                        }
+                        Add(ActiveRoutine = new Coroutine(DeActivate()));
+                    }
+                }
+            }
             if (CollideCheck<Solid>(Position))
             {
                 Rectangle LeftHit = size == "Small" ? new Rectangle((int)Position.X + 1, (int)Position.Y + 7, 1, height) : size == "Medium" ? new Rectangle((int)Position.X - 1, (int)Position.Y - 2, 1, height) : new Rectangle((int)Position.X - 3, (int)Position.Y - 11, 1, height);
@@ -89,7 +153,7 @@ namespace Celeste.Mod.XaphanHelper.Entities
                 {
                     MoveH(1);
                 }
-                else if (Scene.CollideCheck<Solid>(RightHit) && !TurnAroundRoutine.Active)
+                else if (Scene.CollideCheck<Solid>(RightHit))
                 {
                     MoveH(-1);
                 }
@@ -144,9 +208,65 @@ namespace Celeste.Mod.XaphanHelper.Entities
                     }
                     yield return null;
                 }
-                Collider.Height = Height;
-                sprite.FlipX = goLeft;
                 sprite.Play("walk");
+            }
+        }
+
+        private IEnumerator Activate()
+        {
+            sprite.Play("activate");
+            while (sprite.CurrentAnimationFrame <= sprite.CurrentAnimationTotalFrames - 1)
+            {
+                yield return null;
+            }
+            active = true;
+            sprite.Play("walk");
+        }
+
+        private IEnumerator DeActivate(bool immediate = false)
+        {
+            sprite.Play("deactivate");
+            int value = size == "Small" ? 2 : size == "Medium" ? 3 : 4;
+            if (immediate)
+            {
+                Collider.Height -= value;
+                sprite.Position.Y -= value;
+                MoveV(value, 0);
+            }
+            else
+            {
+                while (sprite.CurrentAnimationFrame <= sprite.CurrentAnimationTotalFrames - 1)
+                {
+                    yield return null;
+                }
+            }
+            active = false;
+            sprite.Play("idle");
+        }
+
+        private void onFrameChange(string s)
+        {
+            if (sprite.CurrentAnimationID == "deactivate")
+            {
+                int value = size == "Small" ? 2 : size == "Medium" ? 3 : 4;
+                if (sprite.CurrentAnimationFrame >= sprite.CurrentAnimationTotalFrames - value - 1)
+                {
+                    Logger.Log(LogLevel.Info, "XH", "Frame : " + sprite.CurrentAnimationFrame);
+                    Collider.Height -= 1;
+                    sprite.Position.Y -= 1;
+                    MoveV(1, 0);
+                }
+            }
+            else if(sprite.CurrentAnimationID == "activate")
+            {
+                int value = size == "Small" ? 2 : size == "Medium" ? 3 : 4;
+                if (sprite.CurrentAnimationFrame >= 2 && sprite.CurrentAnimationFrame < 2 + value)
+                {
+                    Logger.Log(LogLevel.Info, "XH", "Frame : " + sprite.CurrentAnimationFrame);
+                    Collider.Height += 1;
+                    sprite.Position.Y += 1;
+                    MoveV(-1, 0);
+                }
             }
         }
 
